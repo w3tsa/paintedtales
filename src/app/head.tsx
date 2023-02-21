7de3e6f2cb32d005ac435d63632a16e8a03870ff
Tanveer Sayem <tanveersayem@gmail.com>
2023-02-20 20:04:40 -0500

    updated the head with &apos;

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Sanaya's Stories</title>
+      <title>Sanaya&apos;s Stories</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Short stories by Sanaya" />
       <link rel="icon" href="/favicon.ico" />
